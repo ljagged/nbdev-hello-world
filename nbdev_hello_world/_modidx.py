51e93deeefcbd4fc369b1be69a3146397865bb88
divergentflow <alex@divergentflow.io>
2026-04-06 18:24:20 -0500

    Added `say_hello`

--- nbdev_hello_world/_modidx.py
+++ nbdev_hello_world/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://ljagged.github.io',
                 'git_url': 'https://github.com/ljagged/nbdev-hello-world',
                 'lib_path': 'nbdev_hello_world'},
-  'syms': {'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')}}}
+  'syms': { 'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')},
+            'nbdev_hello_world.nbdev_hello_world': { 'nbdev_hello_world.nbdev_hello_world.say_hello': ( 'core.html#say_hello',
+                                                                                                        'nbdev_hello_world/nbdev_hello_world.py')}}}
